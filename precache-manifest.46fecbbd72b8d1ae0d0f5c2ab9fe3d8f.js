6872a64faa309575b9df4689fecd135cd5ba6621
Noor Mohammed <mail@noormohammed.tech>
2021-12-08 00:15:19 +0530

    First Commit

---
+++ precache-manifest.46fecbbd72b8d1ae0d0f5c2ab9fe3d8f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da1b8216aa1f215571a1e0176499e9e7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7ebb1715b013805f06d3",
+    "url": "./static/css/2.a6a636a0.chunk.css"
+  },
+  {
+    "revision": "48ba4bf11e18c0aaa2c8",
+    "url": "./static/css/main.d850c1a7.chunk.css"
+  },
+  {
+    "revision": "7ebb1715b013805f06d3",
+    "url": "./static/js/2.e5e08dd5.chunk.js"
+  },
+  {
+    "revision": "b67f3bcb1e8c50265d3b21effa60c84d",
+    "url": "./static/js/2.e5e08dd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48ba4bf11e18c0aaa2c8",
+    "url": "./static/js/main.45594078.chunk.js"
+  },
+  {
+    "revision": "e720711dd3e4e8b209d0",
+    "url": "./static/js/runtime-main.43c7aded.js"
+  },
+  {
+    "revision": "df8b84d5c24a3544c25001f3959d7a77",
+    "url": "./static/media/box.df8b84d5.png"
+  }
+]);
